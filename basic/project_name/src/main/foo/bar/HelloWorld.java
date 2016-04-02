226c1b563ab0a7ec8e96b8dfe7f4f5c74cb13d7d
Fernando Felix <fernandojr.ifcg@live.com>
2016-04-02 02:04:59 -0300

    Hello World code with test.

---
+++ basic/project_name/src/main/foo/bar/HelloWorld.java
@@ -0,0 +1,11 @@
+package foo.bar;
+
+public class HelloWorld {
+	
+	public String message = "Hello World";
+	
+	public static void main(String[] args) {
+		System.out.println( new HelloWorld().message);
+	}
+
+}
