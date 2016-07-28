55326299f25b8872a9d6dacf417928709dcc54f2
Fernando Felix <fernandojr.ifcg@live.com>
2016-07-28 01:37:17 -0300

    Revert HibernateUtil.java

--- project_name/src/main/java/core/HibernateUtil.java
+++ project_name/src/main/java/core/HibernateUtil.java
@@ -1,14 +1,7 @@
 package core;
 
-import java.text.ParseException;
-
-import org.hibernate.QueryException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.metadata.ClassMetadata;
-import org.hibernate.type.Type;
-
-import util.Parser;
 
 import foo.bar.entities.Event;
 
@@ -77,90 +70,6 @@ public class HibernateUtil {
 	 */
 	public static void registerAnnoteatedClass(Configuration configuration) {
 		configuration.addAnnotatedClass(Event.class);
-	}
-
-	/**
-	 * Retorna o tipo da propriedade de uma entidade recursivamente
-	 * 
-	 * @param c
-	 *            A classe da entidade
-	 * @param propertyName
-	 *            O nome da propriedade (ex.: veiculo.modelo.codigo))
-	 * @return O tipo da propriedade
-	 * @throws QueryException
-	 */
-	public static Type getPropertyType(Class<?> c, String propertyName) throws QueryException {
-
-		if (propertyName == null)
-			throw new QueryException("Property name passed cannot be null or empty");
-
-		// se for uma propriedade singular
-		if (!propertyName.contains(".")) {
-			return getClassMetadata(c).getPropertyType(propertyName);
-		} // se não ...
-
-		// seta a propriedade principal (a primeira)
-		Type mainProperty = getClassMetadata(c).getPropertyType(propertyName.split("\\.")[0]);
-
-		// se realmente for uma propriedade e se for um tipo de entidade
-		if (mainProperty.isEntityType()) {
-
-			String subProperties = propertyName.substring(propertyName.indexOf(".") + 1, propertyName.length());
-
-			// verifica se as subpropriedades sao validas pelo seu
-			// gerenciador
-			return getPropertyType(mainProperty.getReturnedClass(), subProperties);
-		} else
-			throw new QueryException("Property " + propertyName.split("\\.")[0] + " is not a entity.");
-	}
-
-	/**
-	 * Retorna os metadados de uma entidade
-	 * 
-	 * @param c
-	 *            A entidade
-	 * @return Se nao for um entidade, retornara null
-	 */
-	@SuppressWarnings("deprecation")
-	public static ClassMetadata getClassMetadata(Class<?> c) {
-		return sessionFactory.getClassMetadata(c);
-	}
-
-	/**
-	 * Converte um valor conforme a classe retornada do tipo de uma propriedade
-	 * de uma determinada entidade
-	 * 
-	 * @param c
-	 *            A entidade
-	 * @param propertyName
-	 *            O nome da propriedade
-	 * @param value
-	 *            O valor a ser convertido
-	 * @return O valor convertido
-	 * @throws ParseException
-	 */
-	public static Object parseValue(Class<?> c, String propertyName, Object value) throws ParseException {
-
-		Type type = getPropertyType(c, propertyName);
-
-		if (value.getClass().equals(type.getReturnedClass()))
-			return value;
-		else
-			return Parser.parseValue(type.getReturnedClass(), value);
-	}
-
-	/**
-	 * Coverte um valor conforme a classe retornada de um tipo de propriedade
-	 * 
-	 * @param type
-	 *            O tipo de propriedade
-	 * @param value
-	 *            O valor a ser convertido
-	 * @return O valor convertido
-	 * @throws ParseException
-	 */
-	public static Object parseValue(Type type, Object value) throws ParseException {
-		return Parser.parseValue(type.getReturnedClass(), value);
 	}
 
 }
